58bb599fb1675b22ae48e7fdd7fa6fcaee4d0165
Ethan Klein <ethan@niceness.org>
2023-05-19 20:59:25 -0700

    basic sequencer working

--- src/Sequencer.js
+++ src/Sequencer.js
@@ -2,14 +2,17 @@ import './Sequencer.scss';
 import SequencerNode from './SequencerNode';
 import { useState, useEffect } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
-import sound1 from './sounds/drums1.wav'
-import kick1 from './sounds/kick1.wav'
+import sound1 from './sounds/drums1.wav';
+import kick1 from './sounds/kick1.wav';
+import snare1 from './sounds/snare1.wav';
 
 
 function Sequencer() {
 
-  const [sequencerOn, setSequencerOn] = useState(false);  
-  const [currentNodeNum, setCurrentNodeNum] = useState(1);
+  const [sequencerOn, setSequencerOn] = useState(false);
+  const [currentNodeNum, setCurrentNodeNum] = useState(0);
+  const [selectedKicks, setSelectedKicks] = useState([]);
+  const [selectedSnares, setSelectedSnares] = useState([]);
 
 
   // sequencer setInterval
@@ -31,25 +34,58 @@ function Sequencer() {
 
     //Clearing the interval
     return () => clearInterval(interval);
-    }, [sequencerOn, currentNodeNum]);
+  }, [sequencerOn, currentNodeNum]);
 
 
   function startSequencer() {
     setSequencerOn(true);
   }
 
+  function addorRemoveKick(index) {
+    if (selectedKicks.indexOf(index) > -1) {
+        // remove from array
+        let newArray = selectedKicks.filter((num) => num !== index)
+        setSelectedKicks(newArray);
+    } else {
+        // add to array
+        setSelectedKicks([
+            ...selectedKicks,
+            index
+          ]);
+    }
+  }
+
+  function addorRemoveSnare(index) {
+    if (selectedSnares.indexOf(index) > -1) {
+        // remove from array
+        let newArray = selectedSnares.filter((num) => num !== index)
+        setSelectedSnares(newArray);
+    } else {
+        // add to array
+        setSelectedSnares([
+            ...selectedSnares,
+            index
+          ]);
+    }
+  }
+
   function stopSequencer() {
     setSequencerOn(false);
-    setCurrentNodeNum(1);
+    setCurrentNodeNum(0);
   }
 
   return (
     <div className="sequencer-container">
-        {[...Array(16)].map((x, i) =>
-            <SequencerNode key={i} currentNodeNum={currentNodeNum} soundFile={kick1} nodeIndex={i}  />
-        )}
-
-        <div style={{color: 'white'}}>{currentNodeNum}</div>
+        <div className='sequencer-row-container'>
+            {[...Array(16)].map((x, i) =>
+                <SequencerNode key={i} onClick={() => {addorRemoveKick(i)}} selectedStatus={selectedKicks.indexOf(i) > -1 ? true : false} currentNodeNum={currentNodeNum} soundFile={kick1} nodeIndex={i} sequencerStatus={sequencerOn}  />
+            )}
+        </div>
+        <div className='sequencer-row-container'>
+            {[...Array(16)].map((x, i) =>
+                <SequencerNode key={i} onClick={() => {addorRemoveSnare(i)}} selectedStatus={selectedSnares.indexOf(i) > -1 ? true : false} currentNodeNum={currentNodeNum} soundFile={snare1} nodeIndex={i} sequencerStatus={sequencerOn}  />
+            )}
+        </div>
 
         <div className="start-button" onClick={() => startSequencer()}>Start</div>
         <div className="stop-button" onClick={() => stopSequencer()}>Stop</div>
--- src/SequencerNode.js
+++ src/SequencerNode.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './SequencerNode.scss';
 import ReactAudioPlayer from 'react-audio-player';
 
-function SequencerNode({currentNodeNum, nodeIndex, soundFile}) {
+function SequencerNode({currentNodeNum, nodeIndex, soundFile, sequencerStatus, selectedStatus, ...rootDOMAttributes}) {
 
   function playAudio(soundFile) {
      let sound = new Audio(soundFile);
-     if (nodeIndex+1 === currentNodeNum) {
+     if (nodeIndex+1 === currentNodeNum && sequencerStatus && selectedStatus) {
         sound.play();
      }
   }
@@ -14,7 +14,7 @@ function SequencerNode({currentNodeNum, nodeIndex, soundFile}) {
   return (
     <>
         {playAudio(soundFile)}
-        <div className={`sequencer-node ${nodeIndex+1 === currentNodeNum ? "on" : "off"}`} />
+        <div {...rootDOMAttributes} className={`sequencer-node ${nodeIndex+1 === currentNodeNum ? "on" : "off"} ${selectedStatus ? "selected" : ""}`} />
     </>
   );
 }
